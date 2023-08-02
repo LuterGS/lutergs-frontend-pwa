0fdafa17d17fc1999c17292a4ba84a63d61f1e00
LuterGS <koo04034@gmail.com>
2023-08-02 16:59:00 +0900

    [TEST] 알람 권한 얻게끔 변경 및 iOS 용 앱 이미지 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
 
     import {onMount} from "svelte";
-    import {Button} from "@svelteuidev/core";
+    import {Button, Group, Space, Text} from "@svelteuidev/core";
 
     let lessThan400px = false;
-    let status = "default";
+    let status;
     // "default", "denied", "granted"
 
-    onMount(() => {
-        Notification.requestPermission().then(result => status = result);
-    })
+    // onMount(() => {
+    //     Notification.requestPermission().then(result => status = result);
+    // })
 
+    const requestPermission = async() => {
+        status = await Notification.requestPermission();
+    }
     const testNoti = () => {
         console.log("test");
         new Notification("test");
@@ -24,8 +27,13 @@
 
 <body bind:clientWidth={width} bind:clientHeight={height}>
     {#if status === "default"}
-        <h1>omg</h1>
+        <h1>알림 권한을 얻어주세요.</h1>
     {/if}
 
+    <Group position="left">
+        <Button on:click={() => requestPermission()}>알람 권한 얻기</Button>
+        <Text>{status}</Text>
+    </Group>
+    <Space h="md"/>
     <Button on:click={() => testNoti()}>알람테스트</Button>
 </body>
