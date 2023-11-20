1932192bfa34eab0b02d05d0eb07f107233b050f
LuterGS <koo04034@gmail.com>
2023-11-20 10:58:37 +0900

    [FIX] fix typo

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "PUBLIC_BACKEND_SERVE=$PUBLIC_BACKEND_SERVER" > /app/.env
+echo "PUBLIC_BACKEND_SERVER=$PUBLIC_BACKEND_SERVER" > /app/.env
 echo "PUBLIC_PUSH_KEY=$PUBLIC_PUSH_KEY" >> /app/.env
 
 npm ci --omit dev
